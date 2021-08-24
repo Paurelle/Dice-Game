fa8fe37402cf76b1385d22b775cea70103846965
Paurelle Marius <marius.paurelle08@gmail.com>
2021-08-24 17:30:19 +0200

    Add display point ATH

--- Js/dice.js
+++ Js/dice.js
@@ -49,6 +49,37 @@ function checkTurnPlayer(){
 }
 
 
+/*------ Functions ATH ------*/ 
+
+function ATH(player, round, global){
+    let score_round = document.getElementById("score-round-player"+player);
+    let score_global = document.getElementById("score-global-player"+player);
+
+    score_round.innerText = round;
+    score_global.innerText = global;
+    
+    let puce_Player1 = document.getElementById("puce-player-1");
+    let puce_Player2 = document.getElementById("puce-player-2");
+
+    let Player_1_bg = document.getElementById("player-1-bg");
+    let Player_2_bg = document.getElementById("player-2-bg");
+
+    if (checkTurnPlayer() === player1) {
+        puce_Player1.style.visibility = "visible";
+        puce_Player2.style.visibility = "hidden";
+
+        Player_1_bg.style.background = "rgb(247, 247, 247)";
+        Player_2_bg.style.background = "rgb(255, 255, 255)";
+    }
+    else{
+        puce_Player1.style.visibility = "hidden";
+        puce_Player2.style.visibility = "visible";
+
+        Player_1_bg.style.background = "rgb(255, 255, 255)";
+        Player_2_bg.style.background = "rgb(247, 247, 247)";
+        
+    }
+}
 
 function gameRestart(){
     
@@ -59,6 +90,9 @@ function gameRestart(){
     player2.score_global = 0;
     player2.turn = false;
     gameEnd = false;
+    for (let id = 1; id <= 2; id++) {
+        ATH(id, 0, 0);
+    }
 }
 
 
@@ -87,6 +121,7 @@ class Player {
         ATH(this.id, this.score_round, this.score_global);
         if (this.score_global >= 100) {
             changeTurn();
+            ATH(this.id, 0, "Win");
             gameEnd = true;
         }
     }
